ec19048ac4006631eb0db6a62586eb66c646bae4
liabueno <117865464+liabueno@users.noreply.github.com>
2023-05-04 17:51:11 +0000

    commit inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,16 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule], 
 })
 export class HomePage {
-  constructor() {}
+
+  produtos = [
+    {'id': 0, 'nome': 'PS5', 'preco': 4000},
+    {'id': 1, 'nome': 'PS4', 'preco': 2000},
+    {'id': 2, 'nome': 'Super Nes', 'preco': 250}, 
+    {'id': 3, 'nome': 'Polistation', 'preco': 800},
+    {'id': 4, 'nome': 'Raspeberry', 'preco': 150},
+  ];
+
 }
