018cbb51423347fc0d2e9f689ec9fbe40236da15
liabueno <117865464+liabueno@users.noreply.github.com>
2023-06-27 18:57:53 +0000

    Atualizações

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,7 +12,7 @@ import { IonicModule } from '@ionic/angular';
 
 export class HomePage {
   resultado: number = 0; // var que armazena o valor do resultado
-  expressao: Array<string> = []; // array que armazena os números e operadores da expressão
+  expressao: Array<any> = []; // array que armazena os números e operadores da expressão
   exibicaoExpressao: string = ""; // var que armazena a exibição da expressão
   
   limparTudo() {
@@ -21,78 +21,86 @@ export class HomePage {
     this.exibicaoExpressao = ""; // limpa a exibição da expressão
   }
   
- efetuarCalculo() {
-  const pilhaNumeros: Array<number> = []; // pilha para realizar os cálculos
-  let operadorAtual = '+'; // operador inicial é soma
-
-    for (let i = 0; i < this.expressao.length; i++) {
-      const elementoAtual = this.expressao[i]; // obtém o elemento atual da expressão
-
-      if (!isNaN(Number(elementoAtual))) {
-        const numero = Number(elementoAtual); // converte o elemento atual para um número
+  efetuarCalculo() {
+    const guardaNumeros: Array<number> = []; 
+    let operadorAtual = '+'; // inicia o operador com a "soma" 
+    
+    for (let i = 0; i < this.expressao.length; i++) { // leitura de todos os valores usados no cálculo
+      const elementoAtual = this.expressao[i];
+  
+      if (!isNaN(Number(elementoAtual))) { // verifica se é NaN ou não
+        const numero = Number(elementoAtual);
         if (operadorAtual === '+') {
-          pilhaNumeros.push(numero); // adiciona o número à pilha
+          guardaNumeros.push(numero); // adiciona valor, no positivo
         } else if (operadorAtual === '-') {
-          pilhaNumeros.push(-numero); // adiciona o número negativo à pilha
+          guardaNumeros.push(-numero); // adiciona valor no negativo, subtraindo
         } else if (operadorAtual === '*') {
-          const numeroAnterior = pilhaNumeros.pop(); // remove o número anterior da pilha
-          if (numeroAnterior !== undefined) {
+          const numeroAnterior = guardaNumeros.pop(); // remove o último elemento da expressão
+          if (numeroAnterior !== undefined) { // verifica se não é um valor indefinido
             const resultadoMultiplicacao = numeroAnterior * numero; // realiza a multiplicação
-            pilhaNumeros.push(resultadoMultiplicacao); // adiciona o resultado à pilha
+            guardaNumeros.push(resultadoMultiplicacao); // adiciona o resultado ao array
           }
         } else if (operadorAtual === '/') {
-          const numeroAnterior = pilhaNumeros.pop();
+          const numeroAnterior = guardaNumeros.pop();
           if (numeroAnterior !== undefined) {
             const resultadoDivisao = numeroAnterior / numero; // realiza a divisão
-            pilhaNumeros.push(resultadoDivisao);
-          }
-        } else if (operadorAtual === '%') {
-          const numeroAnterior = pilhaNumeros.pop();
-          if (numeroAnterior !== undefined) {
-            const resultadoPorcentagem = numeroAnterior * (numero / 100); // realiza o cálculo da porcentagem
-            pilhaNumeros.push(resultadoPorcentagem);
+            guardaNumeros.push(resultadoDivisao);
           }
         }
-      } else if (typeof elementoAtual === 'string') {
+      } else {
         operadorAtual = elementoAtual; // atualiza o operador atual
+      }
     }
+  
+    let resultado = 0;
+    if (guardaNumeros.length > 0) { // se o tamanho do array for maior que zero
+      resultado = guardaNumeros.reduce((acum, curr) => acum + curr); // calcula a soma dos elementos na var numeros e armazena o resultado nessa var
     }
-
-  this.resultado = pilhaNumeros.reduce((acum, curr) => acum + curr, 0); // calcula o resultado somando os valores da pilha
-  this.exibicaoExpressao = this.expressao.join(" "); // atualiza a exibição da expressão
+  
+    this.resultado = resultado; // armazena o resultado
+    this.exibicaoExpressao = this.expressao.join(" "); // atualiza a exibição da expressão, com espaço
   }
-
+  
   addValores(valor: any, tipo: any) {
     // descobre a que se refere o caractere pelo tipo informado no (click)
     if (tipo === 'numero') {
-      let ultimoElemento = this.expressao[this.expressao.length - 1];
+      let elementoFinal = this.expressao[this.expressao.length - 1]; // seleciona o último valor do array
       // verifica se o último elemento da expressão é uma string que contém um ponto decimal ou se é um número válido
-      if (typeof ultimoElemento === 'string' && (ultimoElemento.includes('.') || !isNaN(Number(ultimoElemento)))) {
-        let novoValor = ultimoElemento + valor; // concatena o valor ao número existente
+      if (typeof elementoFinal === 'string' && (elementoFinal.includes('.') || !isNaN(Number(elementoFinal)))) { // verifica se a var elementoFinal possui ponto (vírgula)
+        let novoValor = elementoFinal + valor; // concatena o valor ao número existente
         this.expressao[this.expressao.length - 1] = novoValor;
       } else {
-        this.expressao.push(valor.toString()); // adiciona um novo número à expressão, caso não
+        this.expressao.push(valor.toString()); // adiciona um novo número à expressão, caso não. força ser string o valor
       }
     } else if (tipo === 'operador') {
-      let ultimoElemento = this.expressao[this.expressao.length - 1];
+      let elementoFinal = this.expressao[this.expressao.length - 1];
       // verifica se o último elemento da expressão não é um dos operadores
-      if (ultimoElemento !== '*' && ultimoElemento !== '/' && ultimoElemento !== '+' && ultimoElemento !== '-' && ultimoElemento !== '%') {
+      if (elementoFinal !== '*' && elementoFinal !== '/' && elementoFinal !== '+' && elementoFinal !== '-' && elementoFinal !== '%') {
         this.expressao.push(valor); // adiciona um novo operador à expressão, caso sim o teste
       }
     } else if (tipo === 'decimal') {
-      let ultimoElemento = this.expressao[this.expressao.length - 1];
-      if (typeof ultimoElemento === 'string' && !ultimoElemento.includes('.')) {
+      let elementoFinal = this.expressao[this.expressao.length - 1];
+      if (typeof elementoFinal === 'string' && !elementoFinal.includes('.')) { // typeof confere e retorna o tipo do caractere
         this.expressao.push('.'); // adiciona um ponto decimal à expressão
-      }
+      } 
     }
-    
-  this.efetuarCalculo();
-  this.exibicaoExpressao = this.expressao.join(' ');
-  }
+    else if (tipo === 'porcentagem'){
+      let elementoFinal = this.expressao[this.expressao.length - 1];
+      let novoElementoFinal = elementoFinal/100; // realiza a porcentagem
+      this.expressao.pop(); 
+      this.expressao.push(novoElementoFinal);
+    }
+      this.efetuarCalculo();
+      this.exibicaoExpressao = this.expressao.join(" ");
+    }
   
   apagarDigito() {
-    this.expressao.pop(); // remove o último elemento da expressão
+    this.expressao.pop(); 
     this.efetuarCalculo(); // realiza o cálculo após a remoção do valor
     this.exibicaoExpressao = this.expressao.join(" "); 
   }
+
 }
+
+// obs:  não precisa colocar o zero ao dividir numeros inteiros por decimais, onde o decimal começa com 0 como em: 2 / .8
+// deixei o zero subentendido nesse caso
